302ebfc6b1ab66b20e7503cf3b65e3191a14a85f
Grey Altr <grey.sifr@gmail.com>
2022-12-08 17:30:22 -0800

    build form eventListener and fetchAndDisplayList funcs in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { buyListItem, createListItem, getListItems } from './fetch-utils.js';
 
 /* Get DOM Elements */
 const form = document.querySelector('.item-form');
@@ -11,4 +12,46 @@ const listEl = document.querySelector('.list');
 
 /* Events */
 
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    await createListItem(item, quantity);
+
+    form.reset();
+
+    fetchAndDisplayList();
+});
+
 /* Display Functions */
+async function fetchAndDisplayList() {
+    const list = await getListItems();
+    listEl.textContent = '';
+    for (let item of list) {
+        const listItemEl = document.createElement('li');
+
+        listItemEl.classList.add('list-item');
+
+        listItemEl.textContent = `${item.quantity}x ${item.item}`;
+
+        if (item.bought) {
+            listItemEl.classList.add('bought');
+        } else {
+            listItemEl.classList.add('not-bought');
+            listItemEl.addEventListener('click', async () => {
+                await buyListItem(item.id);
+                fetchAndDisplayList();
+            });
+        }
+
+        listEl.append(listItemEl);
+    }
+}
+
+window.addEventListener('load', async () => {
+    fetchAndDisplayList();
+});
